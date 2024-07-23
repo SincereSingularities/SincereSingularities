425161548b3d0d08a00c640a78cba3a3e26cabad
WassCodeur <wachioubouraima56@gmail.com>
2024-07-23 13:25:26 +0000

    NF: Add database functionality

---
+++ src/sincere_singularities/data/db.py
@@ -0,0 +1,205 @@
+import json
+import os
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Any
+
+from dotenv import load_dotenv
+from pymongo import MongoClient, errors
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_URI = f"mongodb://{DB_HOST}:{DB_PORT}"
+client = MongoClient(DB_URI)
+
+
+class ConnectError(Exception):
+    """Connection error"""
+
+    def __init__(self, message: str) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class DbClient:
+    """db client"""
+
+    def __init__(self) -> None:
+        self.connected = False
+        try:
+            self.client = MongoClient(DB_URI)
+            self.connected = True
+            self.client.server_info()
+            self.db = client[DB_NAME]
+            self.restaurants = self.db.restaurants
+        except errors.ConnectionFailure as err:
+            self.connected = False
+            print(f"Error: {err}")
+            raise err from None
+
+    def is_connected(self) -> bool:
+        """Check if connected
+
+        Returns:
+            bool: True if connected, False otherwise
+        """
+        return self.connected
+
+    def add_element(self, collection: str, data: dict[str, Any]) -> None:
+        """Add element
+
+        Args:
+            collection (str): Collection name
+            data (dict[str, Any]): Data to add
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+
+        name = data["name"]
+        # Check if the restaurant already exists
+        if not self.db[collection].find_one({"name": name}):
+            self.db[collection].insert_one(data)
+
+    def add_many(self, collection: str, datas: Iterable[Any]) -> None:
+        """Add many elements
+
+        Args:
+            collection (str): Collection name
+            datas (Iterable[Any]): list of Datas to add
+
+        Returns:
+            None
+        """
+        for data in datas:
+            self.add_element(collection, data)
+
+    def delete_all(self, collection: str) -> None:
+        """Delete all elements
+
+        Args:
+            collection (str): Collection name
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+        self.db[collection].delete_many({})
+
+    def delete_many(self, collection: str, datas: Iterable[Any]) -> None:
+        """Delete many elements
+
+        Args:
+            collection (str): Collection name
+            datas (Iterable[Any]): list of datas to delete
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+
+        for data in datas:
+            if self.show_one(collection, data):
+                self.db[collection].delete_one(data)
+
+    def delete_one(self, collection: str, data: dict[str, Any]) -> None:
+        """Delete one element
+
+        Args:
+            collection (str): Collection name
+            data (dict[str, Any]): Data to delete
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+        if self.show_one(collection, data):
+            self.db[collection].delete_one(data)
+
+    def show_all(self, collection: str) -> Iterable[Any]:
+        """Show all elements
+
+        Args:
+            collection (str): Collection name
+
+        Returns:
+            Iterable[Any]: All elements in the collection
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+        return list(self.db[collection].find({}))
+
+    def show_one(self, collection: str, data: dict[str, Any]) -> dict[str, Any]:
+        """Show one element
+
+        Args:
+            collection (str): Collection name
+            data (dict[str, Any]): Data to show
+
+        Returns:
+            dict[str, Any]: Element found
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+
+        return dict(self.db[collection].find_one(data))
+
+    def update_one(self, collection: str, data: dict[str, Any], new_data: dict[str, Any]) -> None:
+        """Update one element in the collection
+
+        Args:
+            collection (str): Collection name
+            data (dict[str, Any]): Data to update
+            new_data (dict[str, Any]): New data to update
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+
+        if self.show_one(collection, data):
+            self.db[collection].update_one(data, {"$set": new_data})
+
+    def update_many(self, collection: str, datas: Iterable[Any], new_datas: Iterable[Any]) -> None:
+        """Update many elements in the collection
+
+        Args:
+            collection (str): Collection name
+            datas (Iterable[Any]): list of datas to update
+            new_datas (Iterable[Any]): list of new datas to update
+
+        Returns:
+            None
+        """
+        if not self.connected:
+            raise ConnectError("Not connected to the database")
+
+        for data, new_data in zip(datas, new_datas, strict=False):
+            if self.show_one(collection, data):
+                self.db[collection].update_one(data, {"$set": new_data})
+
+
+if __name__ == "__main__":
+    db = DbClient()
+    db = DbClient()
+    path = Path("src/sincere_singularities/data/restaurants.json")
+
+    with path.open() as f:
+        restaurants = json.load(f)
+
+    db.add_many("restaurants", restaurants)
+    print(db.show_all("restaurants"))
+
+    print(db.show_all("restaurants"))
